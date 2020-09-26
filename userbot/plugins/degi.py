64a4454fbf2b909321ce519b17a2e1ddafba28d9
silverhalobit <71850514+silverhalobit@users.noreply.github.com>
2020-09-26 20:28:47 +0530

    Update degi.py

--- userbot/plugins/degi.py
+++ userbot/plugins/degi.py
@@ -1,36 +1,36 @@
 """Fun pligon...for @PepeB0t
 \nCode by @kirito6969 , ©[Eyepatch](https://t.me/NeoMatrix90)
-type `.degi` and `.nehi` to see the fun.
+type `.cat` and `.milli` to see the fun.
 """
 import random, re
 from uniborg.util import admin_cmd
 import asyncio
 from telethon import events
 
-@borg.on(admin_cmd(pattern="degi ?(.*)"))
+@borg.on(admin_cmd(pattern="cat ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("Wo")
+        await event.edit("i")
         await asyncio.sleep(0.7)
-        await event.edit("Degi")
+        await event.edit("love")
         await asyncio.sleep(1)
-        await event.edit("Tum")
+        await event.edit("you")
         await asyncio.sleep(0.8)
-        await event.edit("Ekbar")
+        await event.edit("milli")
         await asyncio.sleep(0.9)
-        await event.edit("Mang")
+        await event.edit("i")
         await asyncio.sleep(1)
-        await event.edit("Kar")
+        await event.edit("love")
         await asyncio.sleep(0.8)
-        await event.edit("Toh")
+        await event.edit("uso")
         await asyncio.sleep(0.7)
-        await event.edit("Dekho")
+        await event.edit("much")
         await asyncio.sleep(1)
-        await event.edit("`Wo Degi Tum Ekbar Mang Kar toh Dekho`")
+        await event.edit("`i love you milli i love uso much`")
 
-@borg.on(events.NewMessage(pattern=r"\.nehi", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.milli", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("`Wo PaKkA DeGi Tu ManG KaR ToH DekH`")
+    await event.edit("`i love you milli i love uso much`")
     await asyncio.sleep(999)
