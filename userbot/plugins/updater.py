98ef9110d59a82b50aae943ec0bc144713bdc389
Hell Pokemon <72679813+hell-pokemon@users.noreply.github.com>
2020-10-12 17:08:34 +0530

    May Work

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -1,8 +1,3 @@
-#"""Update UserBot Code (FOR DARKCOBRA USERBOT)
-#Syntax: .update
-#\nAll Credits goes to © @hellboi_atul
-#\nFor this awasome plugin.\nPorted from PpaperPlane Extended"""
-
 from os import remove, execle, path, makedirs, getenv, environ, execl
 from shutil import rmtree
 import asyncio
